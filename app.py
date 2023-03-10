10fbc712a34a29d5c43e4851ee5ecc3f2da7c9f8
Ritesh2602 <riteshtahilramani1@gmail.com>
2023-03-10 14:11:18 +0530

    Made an integration of mysql to login and signup page

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
+from flask_sqlalchemy import SQLAlchemy
+import mysql.connector
 
 app=Flask(__name__)
-
+conn = mysql.connector.connect(
+   user='root', password='', host='127.0.0.1', database='login'
+)
+#Creating a cursor object using the cursor() method
+cursor = conn.cursor()
 
 # decorator
 @app.route('/')
@@ -11,6 +17,41 @@ def login():
 @app.route('/register')
 def register():
     return render_template('register.html')
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/login_validation', methods=['POST','GET'])
+
+def login_validation():
+    message=''
+    email=request.form.get('email')
+    password=request.form.get('password')
+
+    query=(""" Select * from `user` WHERE `email` LIKE '{}' AND `password` LIKE '{}' """.format(email,password))
+    cursor.execute(query)
+    user=cursor.fetchall()
+    # print(user)
+    # return user
+
+    if user:
+        return render_template('home.html')
+    else:
+        message="Enter correct details"
+        return render_template('login.html',message=message)
+    
+
+@app.route('/add_user',methods=['POST'])
+def add_user():
+    email=request.form.get('email')
+    phone=request.form.get('phone')
+    password=request.form.get('password')
+
+    cursor.execute("""INSERT into `user` (`userid`,`email`,`phone`,`password`) VALUES (NULL,'{}','{}','{}') """.format(email,phone,password))
+    # cursor.execute(query)
+    conn.commit()
+    return "User registered successfully"
 
 
 # print(__name__)
